0a1d411bd02204c442ecde8b108afc33a816e954
ninamartila <ninamartila03@gmail.com>
2021-08-26 12:03:43 +0700

    add api url

--- tour-schedule/src/apis/axiosApi.js
+++ tour-schedule/src/apis/axiosApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/";
+const API_URL = "https://tour-schedule.herokuapp.com/";
 
 const axiosApi = axios.create({
   baseURL: API_URL,
